ee6ba67a5c9e54b149bccbf2e38ae65bdcd90f1a
mathilde-chenillot <mathilde.cheni@gmail.com>
2022-04-28 22:54:09 +0200

    signup et login methods, but problem with signup

--- app/router.js
+++ app/router.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const userController = require('./controllers/userController');
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.json({ status: 200, message : 'ok'})
 });
+
+router.post('/user/signup', userController.signup);
+router.post('/user/login', userController.login);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'));
 
-app.use(router);
+app.use('/api', router);
 
 app.listen(PORT, () => {
     console.log(`app : http://localhost:${PORT}`);
