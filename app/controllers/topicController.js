f616d9c5b0cb2e8ead1bf0ed9f475b627c3cc4de
mathilde-chenillot <mathilde.cheni@gmail.com>
2022-05-09 15:02:50 +0200

    modify controller getOneTopic

--- app/controllers/topicController.js
+++ app/controllers/topicController.js
@@ -13,8 +13,13 @@ const topicController = {
     getOneTopic : async (req, res) => {
         try {
     
-            const topic = await Topic.findOne({ _id: req.params.id });
-            res.status(200).json(topic);
+            const topic = await Topic.findById(req.params.id);
+
+            if(topic) {
+                res.status(200).json(topic);
+            } else {
+                res.status(404).json('Ce topic n\'existe pas')
+            };
     
         } catch (error) {
             res.status(500).send(error.message);
